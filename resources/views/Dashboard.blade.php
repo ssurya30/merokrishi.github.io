c07d87e7f947fe9ffa4154e349fd0308aea3dd0f
unknown <jaisisurya30@gmail.com>
2022-10-05 09:59:57 +0545

    commit

---
+++ resources/views/Dashboard.blade.php
@@ -0,0 +1,23 @@
+<header>
+    <li >Home</li>
+    <li>Krishi Technology</li>
+    <li>Krishi Market</li>
+    <li>Krishi Guide</li>
+    <li>Krishi Insurance</li>
+</header>
+<br>
+<h1>Login Data Fetch</h1>
+<label>First Name:</label>
+<input type="text" placeholder="Enter your First Name"/><br><br>
+<label>Last Name:</label>
+<input type="text" placeholder="Enter your Last Name" /><br><br>
+<label>UserName:</label>
+<input type="text" placeholder="Enter your UserName"/><br><br>
+<label>Phone Number:</label>
+<input type="text" placeholder="Enter your Phone Number" /><br><br>
+<label>Password:</label>
+<input type="text" placeholder="Enter your  Password"/> <br><br><br>
+<button type="submit" name="send" value="Send" >Send</button><br> <br>
+<div id="weatherapi-weather-widget-5"></div><script type='text/javascript' src='https://www.weatherapi.com/weather/widget.ashx?loc=1774062&wid=5&tu=2&div=weatherapi-weather-widget-5' async></script><noscript><a href="https://www.weatherapi.com/weather/q/kathmandu-1774062" alt="Hour by hour Kathmandu weather">10 day hour by hour Kathmandu weather</a></noscript>
+
+
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
+use App\Http\Controllers\merokrishicontroller;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/Dashboard', function () {
+    return view('Dashboard');
+});
+Route::get('users',[merokrishicontroller::class,'index']); 
+    
+//Route:get('users',[merokrishicontroller::class,'index']);
